67f8a653d6bcd477cb0434d582127127113ec5f6
dheeraz-lab <dheerazys@gmail.com>
2025-06-22 12:58:41 +0530

    Update SpringbootAwsDeployApplicationTests.java

--- src/test/java/com/example/springbootawsdeploy/SpringbootAwsDeployApplicationTests.java
+++ src/test/java/com/example/springbootawsdeploy/SpringbootAwsDeployApplicationTests.java
@@ -1,13 +1,13 @@
 package com.example.springbootawsdeploy;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootTest
-class SpringbootAwsDeployApplicationTests {
+@RestController
+public class HelloController {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @GetMapping("/")
+    public String index() {
+        return "Application is running!";
+    }
 }
